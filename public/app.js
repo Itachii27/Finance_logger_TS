3892a0fb4c12b611d469e91d9a3e334ad3ef7efe
SurbhiB28 <surbhib@crowdstaffing.com>
2022-04-28 05:35:31 +0530

    Rendering HTML Template

--- public/app.js
+++ public/app.js
@@ -22,8 +22,6 @@ const greetPerson = (person: IsPerson) => {
 }
 greetPerson(me)
 */
-import { Invoice } from './classes/Invoice.js';
-import { Payment } from './classes/Payment.js';
 /*
 let docOne: HasFormatter
 let docTwo: HasFormatter
@@ -56,11 +54,19 @@ invoices.forEach(inv => {
 //   console.log(anchor.href);
 //}
 //console.log(anchor.href)
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -70,5 +76,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
